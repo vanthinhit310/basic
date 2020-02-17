6ce84c1cbd6f5f8b670180b1ebc8dfd92e9e5015
Anonymous <vanthinhit310@gmail.com>
2020-02-17 19:36:52 +0700

    -

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,20 @@
+@php $version = time(); @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="{{ setting('seo.seo_description') ?? '' }}">
-    <meta name="keywords" content="{{ setting('seo.meta_keyword') ?? ''}}">
-    <meta name="author" content="{{ setting('seo.web_author') ?? '' }}">
-    <link rel="icon" href="{{ Voyager::image(setting('site.favicon')) ?? '' }}" type="image/png" sizes="128x128">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="" type="image/png" sizes="128x128">
+    <link rel="stylesheet" href="{{ asset('css/common.css?version='.$version) }}">
     <title>
         @hasSection('pageTitle')
-            @yield('pageTitle') | {{ setting('site.title') ?? ''}}
+            @yield('pageTitle')
         @else
-            {{ setting('site.title')  ?? ''}}
+            Page
         @endif
     </title>
 
@@ -26,7 +28,7 @@
         @yield('content')
     </div>
     <!-- Scripts -->
-    <script src="{{ asset('js/common.js') }}"></script>
+    <script src="{{ asset('js/common.js?version='.$version) }}"></script>
     @stack('script')
 </body>
 </html>
