880989fb660b4928d0f32422f03d9c9f15f52ca6
Anonymous <vanthinhit310@gmail.com>
2020-02-17 20:37:58 +0700

    no message

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
     <link rel="icon" href="" type="image/png" sizes="128x128">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css?version='.$version) }}">
     <link rel="stylesheet" href="{{ asset('css/common.css?version='.$version) }}">
     <title>
         @hasSection('pageTitle')
@@ -32,6 +33,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"></script>
     <script src="{{ asset('js/common.js?version='.$version) }}"></script>
+    <script src="{{ asset('js/app.js?version='.$version) }}"></script>
     @stack('script')
 </body>
 </html>
